0ff6164d0df67131b1172ce3d3e68e2264a9235a
grandonk666 <grandonk457@gmail.com>
2021-07-23 23:52:49 +0700

    improving performance

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -2,7 +2,7 @@ import Loader from "react-loader-spinner";
 
 const LoadingSpinner = ({ label }) => {
   return (
-    <div className="w-ful h-full flex flex-col justify-center items-center">
+    <div className="w-ful h-screen flex flex-col justify-center items-center">
       <Loader type="Audio" color="#1F2937" height={250} width={250} />
       <h2 className="text-2xl md:text-3xl text-center font-semibold">
         {label}
