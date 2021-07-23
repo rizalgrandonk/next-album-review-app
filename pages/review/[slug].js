823cff7b16615f58d4aa8c4a84a76113b0ff8a29
grandonk666 <grandonk457@gmail.com>
2021-07-23 23:49:57 +0700

    improving performance

--- pages/review/[slug].js
+++ pages/review/[slug].js
@@ -1,62 +1,51 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
 import Meta from "../../components/Meta";
 import styles from "./Review.module.css";
+import LoadingSpinner from "../../components/LoadingSpinner";
 
-const Review = ({ review }) => {
-  return (
-    <>
-      <Meta title={review.title} />
-      <section>
-        <div
-          className="w-full h-64 md:h-96 bg-cover bg-center"
-          style={{
-            backgroundImage: `url(${review.selectedFile})`,
-          }}
-        ></div>
-        <div className="container mx-auto py-8 px-6 md:px-16">
-          <h1 className="text-3xl md:text-5xl font-semibold mb-6 text-gray-800">
-            {review.title}
-          </h1>
-          <p className="text-lg md:text-xl text-gray-700 my-1">
-            A review by <span className="font-semibold">{review.author}</span>
-          </p>
-          <p className="md:text-lg font-medium my-1">{review.album}</p>
-          <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
-            {review.band}
-          </span>
-          <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
-            {review.genre}
-          </span>
+const Review = () => {
+  const router = useRouter();
+  const slug = router.query.slug;
+
+  const { data: review } = useSWR(`/api/review/${slug}`);
+
+  if (!review) {
+    return <LoadingSpinner label="Loading Content" />;
+  } else {
+    return (
+      <>
+        <Meta title={review.title} />
+        <section>
           <div
-            className={styles.content}
-            dangerouslySetInnerHTML={{ __html: review.content }}
-          />
-        </div>
-      </section>
-    </>
-  );
+            className="w-full h-64 md:h-96 bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${review.selectedFile})`,
+            }}
+          ></div>
+          <div className="container mx-auto py-8 px-6 md:px-16">
+            <h1 className="text-3xl md:text-5xl font-semibold mb-6 text-gray-800">
+              {review.title}
+            </h1>
+            <p className="text-lg md:text-xl text-gray-700 my-1">
+              A review by <span className="font-semibold">{review.author}</span>
+            </p>
+            <p className="md:text-lg font-medium my-1">{review.album}</p>
+            <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
+              {review.band}
+            </span>
+            <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
+              {review.genre}
+            </span>
+            <div
+              className={styles.content}
+              dangerouslySetInnerHTML={{ __html: review.content }}
+            />
+          </div>
+        </section>
+      </>
+    );
+  }
 };
 
 export default Review;
-
-export async function getServerSideProps(context) {
-  const { slug } = context.params;
-
-  try {
-    const res = await fetch(
-      `https://albumreview.vercel.app/api/review/${slug}`
-    );
-    const review = await res.json();
-
-    return {
-      props: { review },
-    };
-  } catch (err) {
-    console.log(err);
-
-    context.res.statusCode = 302;
-    context.res.setHeader("Location", `/`);
-    return {
-      notFound: true,
-    };
-  }
-}
--- pages/review/edit/[slug].js
+++ pages/review/edit/[slug].js
@@ -1,35 +1,43 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import ReviewForm from "../../../components/ReviewForm";
+import LoadingSpinner from "../../../components/LoadingSpinner";
 
-const EditReview = ({ review }) => {
+const EditReview = () => {
+  const router = useRouter();
+  const slug = router.query.slug;
+
+  const { data: review } = useSWR(`/api/review/${slug}`);
+
   return (
     <section className="container mx-auto py-12 px-3">
       <h1 className="text-3xl md:text-4xl font-medium mb-8">Edit review</h1>
-      <ReviewForm review={review} />
+      {!review ? <LoadingSpinner /> : <ReviewForm review={review} />}
     </section>
   );
 };
 
 export default EditReview;
 
-export async function getServerSideProps(context) {
-  const { slug } = context.params;
+// export async function getServerSideProps(context) {
+//   const { slug } = context.params;
 
-  try {
-    const res = await fetch(
-      `https://albumreview.vercel.app/api/review/${slug}`
-    );
-    const review = await res.json();
+//   try {
+//     const res = await fetch(
+//       `https://albumreview.vercel.app/api/review/${slug}`
+//     );
+//     const review = await res.json();
 
-    return {
-      props: { review },
-    };
-  } catch (err) {
-    console.log(err);
+//     return {
+//       props: { review },
+//     };
+//   } catch (err) {
+//     console.log(err);
 
-    context.res.statusCode = 302;
-    context.res.setHeader("Location", `/`);
-    return {
-      notFound: true,
-    };
-  }
-}
+//     context.res.statusCode = 302;
+//     context.res.setHeader("Location", `/`);
+//     return {
+//       notFound: true,
+//     };
+//   }
+// }
