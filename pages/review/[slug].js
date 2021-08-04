eab34546207aa764b97fd6d3b0af0a2829ff0042
grandonk666 <grandonk457@gmail.com>
2021-08-04 19:19:30 +0700

    improving layout

--- pages/review/[slug].js
+++ pages/review/[slug].js
@@ -24,21 +24,31 @@ const Review = () => {
             }}
           ></div>
           <div className="container mx-auto py-8 px-6 md:px-16">
-            <h1 className="text-3xl md:text-5xl font-semibold mb-6 text-gray-800">
-              {review.title}
-            </h1>
-            <p className="text-lg md:text-xl text-gray-700 my-1">
-              A review by <span className="font-semibold">{review.author}</span>
-            </p>
-            <p className="md:text-lg font-medium my-1">{review.album}</p>
-            <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
-              {review.band}
-            </span>
-            <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
-              {review.genre}
-            </span>
+            <div className="w-full flex flex-col md:flex-row justify-between items-start border-b-2 border-gray-300 pb-6">
+              <div>
+                <h1 className="text-3xl md:text-5xl font-semibold mb-6 text-gray-800">
+                  {review.title}
+                </h1>
+                <p className="text-lg md:text-xl text-gray-700 my-1">
+                  A review by{" "}
+                  <span className="font-semibold">{review.author}</span>
+                </p>
+              </div>
+              <div>
+                <p className="md:text-lg md:text-right font-medium my-1">
+                  {review.album}
+                </p>
+                <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
+                  {review.band}
+                </span>
+                <span className="text-sm md:text-base inline-block font-medium mr-2 my-1 px-3 rounded text-gray-200 bg-gray-800">
+                  {review.genre}
+                </span>
+              </div>
+            </div>
+
             <div
-              className={styles.content}
+              className={`${styles.content} pt-2 md:px-48`}
               dangerouslySetInnerHTML={{ __html: review.content }}
             />
           </div>
